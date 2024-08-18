ced229a2d4dfad45435beb8f19b817fc0eeb370b
hanip303 <hanipalhapidz56@gmail.com>
2024-08-18 08:29:11 +0700

    Try fix / page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,5 @@
 import Header from "./components/header";
 import { getCurrentUser } from "@/lib/auth";
-import { DataGrid } from "./components/data-grid";
-import LatestPengajuan from "./components/latest-pengajuan";
-import PengajuanChart from "./components/pengajuan-chart";
-import prisma from "@/db/prisma";
 import { Suspense } from "react";
 import MainDashboard, {
   MainDashboardSkeleton,
@@ -11,66 +7,6 @@ import MainDashboard, {
 
 export default async function Home() {
   const user = await getCurrentUser();
-
-  const suratKelahiranMasuk = await prisma.suratKelahiran.count();
-  const suratKematianMasuk = await prisma.suratKematian.count();
-  const skBelumMenikahMasuk = await prisma.skBelumMenikah.count();
-  const skPenghasilanOrangTuaMasuk = await prisma.skPenghasilanOrangTua.count();
-  const skIjinKeramaianMasuk = await prisma.skIjinKeramaian.count();
-
-  const suratKelahiranDiterima = await prisma.suratKelahiran.count({
-    where: { status: "DITERIMA" },
-  });
-  const suratKematianDiterima = await prisma.suratKematian.count({
-    where: { status: "DITERIMA" },
-  });
-  const skBelumMenikahDiterima = await prisma.skBelumMenikah.count({
-    where: { status: "DITERIMA" },
-  });
-  const skPenghasilanOrangTuaDiterima =
-    await prisma.skPenghasilanOrangTua.count({
-      where: { status: "DITERIMA" },
-    });
-  const skIjinKeramaianDiterima = await prisma.skIjinKeramaian.count({
-    where: { status: "DITERIMA" },
-  });
-
-  const suratKelahiranDitolak = await prisma.suratKelahiran.count({
-    where: { status: "DITOLAK" },
-  });
-  const suratKematianDitolak = await prisma.suratKematian.count({
-    where: { status: "DITOLAK" },
-  });
-  const skBelumMenikahDitolak = await prisma.skBelumMenikah.count({
-    where: { status: "DITOLAK" },
-  });
-  const skPenghasilanOrangTuaDitolak = await prisma.skPenghasilanOrangTua.count(
-    {
-      where: { status: "DITOLAK" },
-    }
-  );
-  const skIjinKeramaianDitolak = await prisma.skIjinKeramaian.count({
-    where: { status: "DITOLAK" },
-  });
-
-  const jumlahSuratMasuk =
-    suratKelahiranMasuk +
-    suratKematianMasuk +
-    skBelumMenikahMasuk +
-    skPenghasilanOrangTuaMasuk +
-    skIjinKeramaianMasuk;
-  const jumlahSuratDiterima =
-    suratKelahiranDiterima +
-    suratKematianDiterima +
-    skBelumMenikahDiterima +
-    skPenghasilanOrangTuaDiterima +
-    skIjinKeramaianDiterima;
-  const jumlahSuratDitolak =
-    suratKelahiranDitolak +
-    suratKematianDitolak +
-    skBelumMenikahDitolak +
-    skPenghasilanOrangTuaDitolak +
-    skIjinKeramaianDitolak;
 
   return (
     <main>
